0140286443437d1b7453b9cf6a4420ea09d3167a
Ryohei Ikegami <iofg2100@gmail.com>
2016-08-17 18:44:44 +0900

    Revert "Update receive-tablet-event"
    This reverts commit b2f81dfe37b40a92a1d7e39fc5e52581d62768e8.

--- src/renderer/views/DrawArea.tsx
+++ src/renderer/views/DrawArea.tsx
@@ -5,10 +5,9 @@ import Tool from "../models/Tool"
 import BrushTool from "../models/BrushTool"
 import Pointer from "../models/Pointer"
 import * as Electron from "electron"
+import {TabletEvent} from "receive-tablet-event"
 
-const {ipcRenderer, remote} = Electron
-const receiveTabletEvent = remote.require("receive-tablet-event")
-import {TabletEvent, TabletEventReceiver} from "receive-tablet-event";
+const {ipcRenderer} = Electron
 
 interface DrawAreaState {
   picture: Picture
@@ -19,6 +18,7 @@ class DrawArea extends React.Component<void, DrawAreaState> {
   element: HTMLElement|undefined
   isPressed = false
   tool: Tool = new BrushTool()
+  usingTablet = false
 
   constructor() {
     super()
@@ -32,27 +32,19 @@ class DrawArea extends React.Component<void, DrawAreaState> {
     this.element = this.refs["root"] as HTMLElement
     this.updateChildCanvases()
 
-    const receiver: TabletEventReceiver = receiveTabletEvent(remote.getCurrentWindow())
-    const rect = this.element.getBoundingClientRect()
-    receiver.captureArea = {
-      left: Math.round(rect.left),
-      top: Math.round(rect.top),
-      width: Math.round(rect.width),
-      height: Math.round(rect.height),
-    }
-
-    receiver.on("down", (ev: TabletEvent) => {
+    ipcRenderer.on("tablet.down", (event: Electron.IpcRendererEvent, ev: TabletEvent) => {
+      this.usingTablet = true
       const pos = this.mousePos(ev)
       this.tool.start(new Pointer(pos, ev.pressure))
       this.isPressed = true
     })
-    receiver.on("move", (ev: TabletEvent) => {
-      if (this.isPressed) {
+    ipcRenderer.on("tablet.move", (event: Electron.IpcRendererEvent, ev: TabletEvent) => {
+      if (this.usingTablet && this.isPressed) {
         const pos = this.mousePos(ev)
         this.tool.move(new Pointer(pos, ev.pressure))
       }
     })
-    receiver.on("up", (ev: TabletEvent) => {
+    ipcRenderer.on("tablet.up", (event: Electron.IpcRendererEvent, ev: TabletEvent) => {
       if (this.isPressed) {
         this.tool.end()
         this.isPressed = false
@@ -91,12 +83,20 @@ class DrawArea extends React.Component<void, DrawAreaState> {
   }
 
   onMouseDown(ev: MouseEvent) {
+    if (this.usingTablet) {
+      return
+    }
+
     const pos = this.mousePos(ev)
     this.tool.start(new Pointer(pos, 1))
     this.isPressed = true
     ev.preventDefault()
   }
   onMouseMove(ev: MouseEvent) {
+    if (this.usingTablet) {
+      return
+    }
+
     const pos = this.mousePos(ev)
 
     if (this.isPressed) {
