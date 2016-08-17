b2f81dfe37b40a92a1d7e39fc5e52581d62768e8
Ryohei Ikegami <iofg2100@gmail.com>
2016-08-17 18:19:32 +0900

    Update receive-tablet-event

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,8 +2,6 @@ import Electron = require('electron')
 type BrowserWindow = Electron.BrowserWindow
 const {app, BrowserWindow} = Electron
 
-import {TabletEventReceiver} from "receive-tablet-event"
-
 let window: BrowserWindow|undefined
 
 function createWindow () {
@@ -14,24 +12,6 @@ function createWindow () {
 
   win.on('closed', () => {
     window = undefined
-  })
-
-  const receiver = new TabletEventReceiver(win.getNativeWindowHandle())
-
-  receiver.on("enterProximity", (ev) => {
-    win.webContents.send("tablet.enterProximity", ev)
-  })
-  receiver.on("leaveProximity", (ev) => {
-    win.webContents.send("tablet.leaveProximity", ev)
-  })
-  receiver.on("down", (ev) => {
-    win.webContents.send("tablet.down", ev)
-  })
-  receiver.on("move", (ev) => {
-    win.webContents.send("tablet.move", ev)
-  })
-  receiver.on("up", (ev) => {
-    win.webContents.send("tablet.up", ev)
   })
 }
 
