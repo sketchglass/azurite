0dddced15787b04210efa836574a1b7b275ac3a6
Ryohei Ikegami <iofg2100@gmail.com>
2016-08-22 18:14:11 +0900

    Enhance Geometry lib

--- src/lib/Geometry.ts
+++ src/lib/Geometry.ts
@@ -1,3 +1,4 @@
+// for point / size
 export
 class Vec2 {
   constructor(public x: number, public y: number) {
@@ -43,6 +44,7 @@ class Vec2 {
   }
 }
 
+// for color / rectangle
 export
 class Vec4 {
   constructor(public x: number, public y: number, public z: number, public w: number) {
@@ -60,6 +62,15 @@ class Vec4 {
   get a() {
     return this.w
   }
+  get xy() {
+    return new Vec2(this.x, this.y)
+  }
+  get zw() {
+    return new Vec2(this.z, this.w)
+  }
+  get size() {
+    return this.size
+  }
 
   add(a: Vec4) {
     return new Vec4(this.x + a.x, this.y + a.y, this.z + a.z, this.w + a.w)
@@ -75,5 +86,55 @@ class Vec4 {
 
   div(a: number) {
       return new Vec4(this.x / a, this.y / a, this.z / a, this.w / a)
+  }
+}
+
+export
+class Transform {
+  constructor(public m11: number, public m12: number, public m21: number, public m22: number, public dx: number, public dy: number) {
+  }
+
+  merge(other: Transform) {
+    // other * this
+    // [P u] [Q v]   [PQ Pv + u]
+    // [0 1] [0 1] = [0  1     ]
+    const m11 = other.m11 * this.m11 + other.m21 * this.m12
+    const m21 = other.m11 * this.m21 + other.m21 * this.m22
+    const m12 = other.m12 * this.m11 + other.m22 * this.m12
+    const m22 = other.m12 * this.m21 + other.m22 * this.m22
+    const dx = other.m11 * this.dx + other.m21 * this.dy + other.dx
+    const dy = other.m12 * this.dx + other.m22 * this.dy + other.dy
+    return new Transform(m11, m12, m21, m22, dx, dy)
+  }
+
+  invert() {
+    // [P u]^-1   [P^-1 -P^1 * v]
+    // [0 1]    = [0    1       ]
+    const det = this.m11 * this.m22 - this.m12 * this.m21
+    const m11 = this.m22 / det
+    const m12 = -this.m12 / det
+    const m21 = -this.m21 / det
+    const m22 = this.m11 / det
+    const dx = -(m11 * this.dx + m21 * this.dy)
+    const dy = -(m12 * this.dx + m22 * this.dy)
+    return new Transform(m11, m12, m21, m22, dx, dy)
+  }
+
+  toGLData() {
+    return new Float32Array([this.m11, this.m12, 0, this.m21, this.m22, 0, this.dx, this.dy, 1])
+  }
+
+  toString() {
+    return `Transform([${this.m11},${this.m12}],[${this.m21},${this.m22}],[${this.dx},${this.dy}])`
+  }
+
+  static identity = new Transform(1, 0, 0, 1, 0, 0)
+
+  static scale(scale: Vec2) {
+    return new Transform(scale.x, 0, 0, scale.y, 0, 0)
+  }
+
+  static translate(translation: Vec2) {
+    return new Transform(1, 0, 0, 1, translation.x, translation.y)
   }
 }
