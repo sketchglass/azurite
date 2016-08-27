370d9e6e6407de4c9cb9b14597ce5c8e9d0fbd66
Ryohei Ikegami <iofg2100@gmail.com>
2016-08-27 20:05:39 +0900

    WIP implement shaders

---
+++ src/renderer/models/WatercolorTool.ts
@@ -0,0 +1,110 @@
+import {Vec2, Vec4, Transform} from "../../lib/Geometry"
+import Waypoint from "./Waypoint"
+import Tool from "./Tool"
+import {Geometry, Shader, Model, GeometryUsage, Framebuffer} from "../../lib/GL"
+import {context} from "../GLContext"
+
+const sampleVertShader = `
+  precision mediump float;
+
+  uniform float uSampleSize;
+  uniform vec2 uLayerSize;
+
+  attribute vec2 aPosition;
+
+  varying vec2 vCenter;
+
+  void main(void) {
+    vec2 center = floor(aPosition - vec2(0.5)) + vec2(0.5);
+    vCenter = center;
+    gl_Position = vec4(center / uLayerSize * vec2(2.0) - vec2(1.0), 0.0, 1.0);
+    gl_PointSize = uSampleSize;
+  }
+`
+
+const copyOrigFragShader = `
+  precision mediump float;
+
+  uniform float uSampleSize;
+
+  uniform sampler2D uLayer;
+  uniform vec2 uLayerSize;
+
+  varying vec2 vCenter;
+
+  void main(void) {
+    vec2 pos = (gl_PointCoord - vec2(0.5)) * vec2(uSampleSize) + vCenter;
+    gl_FragColor = texture2D(uLayer, pos / uLayerSize);
+  }
+`
+
+const shapeFragShader = `
+  precision mediump float;
+
+  uniform float uBrushRadius;
+  uniform float uSampleSize;
+
+  void main(void) {
+    float r = distance(gl_PointCoord, vec2(0.5)) * uSampleSize;
+    lowp float opacity = smoothstep(uBrushRadius, uBrushRadius - 1.0, r);
+    gl_FragColor = vec4(opacity);
+  }
+`
+
+const sampleFragShader = `
+  precision mediump float;
+
+  uniform sampler2D uOriginal;
+  uniform sampler2D uBrushShape;
+
+  void main(void) {
+    gl_FragColor = texture2D(uOriginal, gl_PointCoord) * vec4(texture2D(uBrushShape, gl_PointCoord).a);
+  }
+`
+
+const brushVertShader = `
+  precision mediump float;
+
+  uniform float uSampleSize;
+  uniform vec2 uLayerSize;
+  attribute vec2 aPosition;
+
+  uniform sampler2D uBrushShape;
+  uniform sampler2D uSample;
+
+  varying lowp vMixColor;
+
+  void main(void) {
+    vec2 center = floor(aPosition - vec2(0.5)) + vec2(0.5);
+    gl_Position = vec4(center / uLayerSize * vec2(2.0) - vec2(1.0), 0.0, 1.0);
+    gl_PointSize = uSampleSize;
+
+    float topLevel = log2(uSampleSize);
+    vMixColor = texture2DLod(uSample, vec2(0.0), topLevel) / vec4(texture2DLod(uBrushShape, vec2(0.0),  topLevel).a);
+  }
+`
+
+const brushFragShader = `
+  precision mediump float;
+
+  uniform float uBlending;
+  uniform float uThickness;
+  uniform lowp vec4 uColor;
+
+  uniform sampler2D uOriginal;
+  uniform sampler2D uBrushShape;
+
+  varying lowp vec4 vMixColor;
+
+  void main(void) {
+    lowp vec4 orig = texture2D(uOriginal, gl_PointCoord);
+    lowp float brushOpacity = texture2D(uOriginal, gl_PointCoord).a;
+
+    lopw float mixRate = brushOpacity * uBlending;
+    // mix color
+    lowp vec4 color = orig * vec4(1.0 - mixRate) + vMixColor * vec4(mixRate);
+    // add color
+    lopw vec4 addColor = uColor * vec4(uThickness * brushOpacity);
+    gl_FragColor = addColor + color * vec4(1.0 - addColor.a);
+  }
+`
