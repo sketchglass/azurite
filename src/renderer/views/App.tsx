9604f34c278404a390397bdbdc5f09438b8f3ff8
minamorl <minamorl@minamorl.com>
2016-09-09 18:37:35 +0900

    now colorpicker works correctly

--- src/renderer/views/App.tsx
+++ src/renderer/views/App.tsx
@@ -1,6 +1,7 @@
 import React = require("react")
 import Picture from "../models/Picture"
 import Tool from "../models/Tool"
+import BaseBrushTool from "../models/BaseBrushTool"
 import BrushTool from "../models/BrushTool"
 import WatercolorTool from "../models/WatercolorTool"
 import BrushSettings from "./BrushSettings"
@@ -8,6 +9,7 @@ import WatercolorSettings from "./WatercolorSettings"
 import DrawArea from "./DrawArea"
 import ColorPicker from "./ColorPicker"
 import {Color} from "../../lib/Color"
+import {Vec4} from "../../lib/Geometry"
 
 function ToolSelection(props: {tools: Tool[], currentTool: Tool, onChange: (tool: Tool) => void}) {
   return (
@@ -20,33 +22,45 @@ function ToolSelection(props: {tools: Tool[], currentTool: Tool, onChange: (tool
   )
 }
 
-interface AppState {
-  brushColor: Color
-}
-
 export default
-class App extends React.Component<void, AppState> {
+class App extends React.Component<void, void> {
   picture = new Picture()
   tools: Tool[] = [new BrushTool(), new WatercolorTool()]
   currentTool = this.tools[0]
+  brushColor: Color
 
-  constructor () {
+  constructor() {
     super()
-    this.state = {
-      brushColor: new Color(0, 0, 0, 0)
+    if(this.currentTool instanceof BaseBrushTool) {
+      const tool = this.currentTool as BaseBrushTool
+      const {r, g, b, a} = tool.color
+      this.brushColor = Color.rgb(Math.round(r * 255), Math.round(g * 255), Math.round(b * 255), a)
     }
   }
-
   render() {
     const {picture, tools, currentTool} = this
     const onToolChange = (tool: Tool) => {
       this.currentTool = tool
+      if(this.currentTool instanceof BaseBrushTool) {
+        const tool = this.currentTool as BaseBrushTool
+        const {r, g, b, a} = tool.color
+        this.brushColor = Color.rgb(Math.round(r * 255), Math.round(g * 255), Math.round(b * 255), a)
+      }
+      this.forceUpdate()
+    }
+    const onBrushColorChange = (color: Color) => {
+      if(!(this.currentTool instanceof BaseBrushTool))
+        return
+      this.brushColor = color
+      const brushTool = this.currentTool as BrushTool
+      const {r, g, b} = color.toRgb()
+      brushTool.color = new Vec4(r/255, g/255, b/255, color.a)
       this.forceUpdate()
     }
     return (
       <div className="app">
         <aside className="sidebar">
-          <ColorPicker color={this.state.brushColor} onChange={(color: Color)=>{this.setState({brushColor: color})}} />
+          <ColorPicker color={this.brushColor} onChange={onBrushColorChange} />
           <ToolSelection tools={tools} currentTool={currentTool} onChange={onToolChange} />
           {currentTool.renderSettings()}
         </aside>
