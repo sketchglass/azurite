941c73dbfee3afc3abaeefaf4fec091d7b2915dc
Ryohei Ikegami <iofg2100@gmail.com>
2016-09-26 17:01:05 +0900

    Add TranslationTool to tools

--- src/renderer/views/App.tsx
+++ src/renderer/views/App.tsx
@@ -4,6 +4,7 @@ import Tool from "../models/Tool"
 import BaseBrushTool from "../models/BaseBrushTool"
 import BrushTool from "../models/BrushTool"
 import WatercolorTool from "../models/WatercolorTool"
+import TranslationTool from "../models/TranslationTool"
 import BrushSettings from "./BrushSettings"
 import WatercolorSettings from "./WatercolorSettings"
 import DrawArea from "./DrawArea"
@@ -31,7 +32,7 @@ function ToolSelection(props: {tools: Tool[], currentTool: Tool, onChange: (tool
 export default
 class App extends React.Component<void, void> {
   picture = new Picture()
-  tools: Tool[] = [new BrushTool(), new WatercolorTool()]
+  tools: Tool[] = [new BrushTool(), new WatercolorTool(), new TranslationTool()]
   currentTool = this.tools[0]
   brushColor: Color
   paletteIndex: number = 0
