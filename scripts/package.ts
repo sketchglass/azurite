7af81c75e54d4c6bc9b9cbc071b1f48a310778ab
Ryohei Ikegami <iofg2100@gmail.com>
2016-10-04 13:02:00 +0900

    Add build script

---
+++ scripts/package.ts
@@ -0,0 +1,34 @@
+const sh = require("shelljs")
+const packager = require('electron-packager')
+
+sh.config.verbose = true
+
+async function package(platform: string) {
+  sh.mkdir('-p', 'build')
+
+  const files = ['package.json', 'dist/index.html']
+  for (const file of files) {
+    sh.cp(file, 'build')
+  }
+
+  sh.cd("build")
+  sh.exec("npm install --production")
+
+  const options = {
+    dir: ".",
+    platform: "win32",
+    arch: "x64",
+  }
+
+  await new Promise<string[]>((resolve, reject) => {
+    packager(options, (err: Error, paths: string[]) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(paths)
+      }
+    })
+  })
+}
+
+package("win32")
