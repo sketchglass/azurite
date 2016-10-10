6e6489cbf169830ab1fb58b4712c437b0a4eb260
Ryohei Ikegami <iofg2100@gmail.com>
2016-10-10 18:51:53 +0900

    Update GLContext

--- src/renderer/GLContext.ts
+++ src/renderer/GLContext.ts
@@ -1,3 +1,3 @@
-import {Context} from "../lib/GL"
+import {Context} from "paintgl"
 export const canvas = document.createElement("canvas")
-export const context = new Context(canvas)
+export const context = new Context(canvas, {preserveDrawingBuffer: true})
