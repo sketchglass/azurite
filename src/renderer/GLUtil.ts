442484e1e0e6fbb19158f8ea8e00239e98e64132
Ryohei Ikegami <iofg2100@gmail.com>
2016-10-10 19:15:10 +0900

    Migrate GLUtil

--- src/renderer/GLUtil.ts
+++ src/renderer/GLUtil.ts
@@ -1,82 +1,33 @@
-import {Vec2, Vec4} from "../lib/Geometry"
-import {Texture, Geometry, GeometryUsage, Shader, Model, Framebuffer, DataType, BlendMode} from "../lib/GL"
+import {Vec2, Rect} from "paintvec"
+import {Texture, RectShape, TextureShader, TextureDrawTarget, PixelType} from "paintgl"
 import {context} from "./GLContext"
 
 export
-const unitGeometry = new Geometry(context, new Float32Array([
-  -1, -1, 0, 0,
-  1, -1, 1, 0,
-  -1, 1, 0, 1,
-  1, 1, 1, 1
-]), [
-  {attribute: "aPosition", size: 2},
-  {attribute: "aTexCoord", size: 2},
-],  new Uint16Array([
-  0, 1, 2,
-  1, 2, 3
-]), GeometryUsage.Static)
-
-const copyTextureShader = new Shader(context,
-  `
-    precision highp float;
-    uniform vec2 uSrcSize;
-    uniform vec2 uDestSize;
-    uniform vec2 uOffset;
-    attribute vec2 aPosition;
-    attribute vec2 aTexCoord;
-    varying vec2 vTexCoord;
-    void main(void) {
-      vTexCoord = aTexCoord;
-      vec2 posSrc = (aPosition + vec2(1.0)) * 0.5 * uSrcSize;
-      vec2 posDest = posSrc - uOffset;
-      vec2 pos = posDest / uDestSize * 2.0 - vec2(1.0);
-      gl_Position = vec4(pos, 0.0, 1.0);
-    }
-  `,
-  `
-    precision mediump float;
-    varying highp vec2 vTexCoord;
-    uniform sampler2D uTexture;
-    void main(void) {
-      gl_FragColor = texture2D(uTexture, vTexCoord);
-    }
-  `
-)
-copyTextureShader.uniform("uTexture").setInt(0)
-const copyTextureUniforms = {
-  uSrcSize: copyTextureShader.uniform("uSrcSize"),
-  uDestSize: copyTextureShader.uniform("uDestSize"),
-  uOffset: copyTextureShader.uniform("uOffset"),
-}
-
-const copyTextureModel = new Model(context, unitGeometry, copyTextureShader)
-copyTextureModel.setBlendMode(BlendMode.Src)
-const workingFramebuffer = new Framebuffer(context)
-
-export
 function copyTexture(src: Texture, dest: Texture, offset: Vec2) {
-  copyTextureUniforms.uSrcSize.setVec2(src.size)
-  copyTextureUniforms.uDestSize.setVec2(dest.size)
-  copyTextureUniforms.uOffset.setVec2(offset)
-  context.textureUnits.set(0, src)
-  workingFramebuffer.setTexture(dest)
-  workingFramebuffer.use()
-  copyTextureModel.render()
-  context.textureUnits.delete(0)
+  const shape = new RectShape(context)
+  shape.rect = new Rect(offset.neg(), offset.neg().add(src.size))
+  shape.shader = TextureShader
+  shape.blendMode = "src"
+  shape.uniforms["texture"] = src
+
+  const target = new TextureDrawTarget(context, dest)
+  target.draw(shape)
+
+  shape.dispose()
+  target.dispose()
 }
 
 export
-function copyNewTexture(src: Texture, rect: Vec4, type: DataType) {
-  const texture = new Texture(context, rect.size, type)
-  copyTexture(src, texture, rect.xy)
+function copyNewTexture(src: Texture, rect: Rect, pixelType: PixelType) {
+  const texture = new Texture(context, {size: rect.size, pixelType})
+  copyTexture(src, texture, rect.topLeft)
   return texture
 }
 
 export
 function readTextureFloat(texture: Texture) {
   const data = new Float32Array(texture.size.width * texture.size.height * 4)
-  workingFramebuffer.setTexture(texture)
-  workingFramebuffer.use()
-  context.readPixelsFloat(Vec4.fromVec2(new Vec2(0), texture.size), data)
+  const target = new TextureDrawTarget(context, texture)
+  target.readPixels(new Rect(new Vec2(), texture.size), data)
   return data
 }
