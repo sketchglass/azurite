1d76eba8cc5e5afdb585c22497fd6e11f7b4a14f
Ryohei Ikegami <iofg2100@gmail.com>
2016-10-16 18:13:23 +0900

    Implement resolution

--- src/dialogs/newPicture.tsx
+++ src/dialogs/newPicture.tsx
@@ -5,16 +5,34 @@ import * as IPCChannels from "../common/IPCChannels"
 import {remote} from "electron"
 import "./newPicture.sass"
 
+type SizeUnits = "px"|"mm"
+
 interface NewPictureDialogState {
-  width: number
-  height: number
+  widthMm: number
+  heightMm: number
+  widthPx: number
+  heightPx: number
+  dpi: number
+  unit: SizeUnits
+}
+
+function mmToPx(mm: number, dpi: number) {
+  return Math.round(mm / 25.4 * dpi)
+}
+
+function pxToMm(px: number, dpi: number) {
+  return Math.round(px / dpi * 25.4)
 }
 
 class NewPictureDialog extends React.Component<{}, NewPictureDialogState> {
   // TODO: save & load last dimension
   state = {
-    width: 1200,
-    height: 800,
+    widthMm: pxToMm(1200, 72),
+    heightMm: pxToMm(800, 72),
+    widthPx: 1200,
+    heightPx: 800,
+    dpi: 72,
+    unit: "px" as SizeUnits
   }
   dialog: HTMLDivElement
 
@@ -26,29 +44,31 @@ class NewPictureDialog extends React.Component<{}, NewPictureDialogState> {
   }
 
   render() {
-    const {width, height} = this.state
+    const {widthMm, heightMm, widthPx, heightPx, dpi, unit} = this.state
+    const width = unit == "mm" ? widthMm : widthPx
+    const height = unit == "mm" ? heightMm : heightPx
 
     return (
       <div className="NewPictureDialog" ref={e => this.dialog = e}>
         <div className="NewPictureDialog_Row">
           <label>Width</label>
-          <input type="number" max={MAX_PICTURE_SIZE} value={width} onChange={this.onWidthChange.bind(this)} />
-          <select>
+          <input type="number" max={MAX_PICTURE_SIZE} value={width} onChange={this.onWidthChange} />
+          <select value={unit} onChange={this.onUnitChange}>
             <option value="px">px</option>
             <option value="mm">mm</option>
           </select>
         </div>
         <div className="NewPictureDialog_Row">
           <label>Height</label>
-          <input type="number" max={MAX_PICTURE_SIZE} value={height} onChange={this.onHeightChange.bind(this)} />
-          <select>
+          <input type="number" max={MAX_PICTURE_SIZE} value={height} onChange={this.onHeightChange} />
+          <select value={unit} onChange={this.onUnitChange}>
             <option value="px">px</option>
             <option value="mm">mm</option>
           </select>
         </div>
         <div className="NewPictureDialog_Row">
           <label>Resolution</label>
-          <input type="number" max={MAX_PICTURE_SIZE} value={72} />
+          <input type="number" max={MAX_PICTURE_SIZE} value={dpi} onChange={this.onDpiChange} />
           DPI
         </div>
         <button onClick={this.onOK.bind(this)}>OK</button>
@@ -56,20 +76,54 @@ class NewPictureDialog extends React.Component<{}, NewPictureDialogState> {
     )
   }
 
-  onWidthChange(ev: Event) {
-    const width = parseInt((ev.target as HTMLInputElement).value)
-    const {height} = this.state
-    this.setState({width, height})
+  onUnitChange = (ev: React.FormEvent<HTMLSelectElement>) => {
+    const unit = (ev.target as HTMLSelectElement).value
+    this.setState({unit} as NewPictureDialogState)
   }
 
-  onHeightChange(ev: Event) {
+  onWidthChange = (ev: React.FormEvent<HTMLInputElement>) => {
+    const width = parseInt((ev.target as HTMLInputElement).value)
+    const {unit, dpi} = this.state
+    if (unit == "mm") {
+      const widthPx = mmToPx(width, dpi)
+      this.setState({widthMm: width, widthPx} as NewPictureDialogState)
+    } else {
+      const widthMm = pxToMm(width, dpi)
+      this.setState({widthMm, widthPx: width} as NewPictureDialogState)
+    }
+  }
+
+  onHeightChange = (ev: React.FormEvent<HTMLInputElement>) => {
     const height = parseInt((ev.target as HTMLInputElement).value)
-    const {width} = this.state
-    this.setState({width, height})
+    const {unit, dpi} = this.state
+    if (unit == "mm") {
+      const heightPx = mmToPx(height, dpi)
+      this.setState({heightMm: height, heightPx} as NewPictureDialogState)
+    } else {
+      const heightMm = pxToMm(height, dpi)
+      this.setState({heightMm, heightPx: height} as NewPictureDialogState)
+    }
+  }
+
+  onDpiChange = (ev: React.FormEvent<HTMLInputElement>) => {
+    const dpi = parseInt((ev.target as HTMLInputElement).value) || 72
+    const {unit} = this.state
+    if (unit == "mm") {
+      const {widthMm, heightMm} = this.state
+      const widthPx = mmToPx(widthMm, dpi)
+      const heightPx = mmToPx(heightMm, dpi)
+      this.setState({dpi, widthPx, heightPx} as NewPictureDialogState)
+    } else {
+      const {widthPx, heightPx} = this.state
+      const widthMm = pxToMm(widthPx, dpi)
+      const heightMm = pxToMm(heightPx, dpi)
+      this.setState({dpi, widthMm, heightMm} as NewPictureDialogState)
+    }
   }
 
   onOK() {
-    IPCChannels.newPictureDialogDone.send(this.state)
+    const {widthPx, heightPx} = this.state
+    IPCChannels.newPictureDialogDone.send({width: widthPx, height: heightPx})
   }
 }
 
