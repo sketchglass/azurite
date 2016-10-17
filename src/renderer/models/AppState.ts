720f4560425d4392b758b5ff8140778ba1a518c1
Ryohei Ikegami <iofg2100@gmail.com>
2016-10-17 23:51:05 +0900

    Split app state from App

---
+++ src/renderer/models/AppState.ts
@@ -0,0 +1,30 @@
+import {observable, computed} from "mobx"
+import Picture from "./Picture"
+import Tool from "./Tool"
+import BrushTool from "./BrushTool"
+import WatercolorTool from "./WatercolorTool"
+import PanTool from "./PanTool"
+import {ZoomTool} from "./ZoomTool"
+import RotateTool from "./RotateTool"
+import {HSVColor} from "../../lib/Color"
+
+export
+class AppState {
+  readonly pictures = observable<Picture>([])
+  @observable currentPictureIndex = 0
+
+  @computed get currentPicture(): Picture|undefined {
+    return this.pictures[this.currentPictureIndex]
+  }
+
+  readonly tools = observable<Tool>([new BrushTool(), new WatercolorTool(), new PanTool(), new ZoomTool(),  new RotateTool()])
+  @observable currentTool: Tool = this.tools[0]
+  @observable overrideTool: Tool|undefined
+
+  @observable color = new HSVColor(0, 0, 1)
+  @observable paletteIndex: number = 0
+  readonly palette = observable<HSVColor>(new Array(100).fill(new HSVColor(0, 0, 1)))
+}
+
+export
+const appState = new AppState()
