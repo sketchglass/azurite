e18ca986290c60e499e71f50921f9ca4e3fe776c
Ryohei Ikegami <iofg2100@gmail.com>
2016-10-17 12:03:17 +0900

    Prevent default space press

--- src/renderer/views/MenuBar.ts
+++ src/renderer/views/MenuBar.ts
@@ -5,17 +5,7 @@ type BrowserWindow = Electron.BrowserWindow
 type MenuItemOptions = Electron.MenuItemOptions
 import Picture from "../models/Picture"
 import PictureExport from "../models/PictureExport"
-
-function isTextInput(elem: Element) {
-  if (elem instanceof HTMLTextAreaElement) {
-    return true
-  }
-  if (elem instanceof HTMLInputElement) {
-    const inputTypes = ['text', 'password', 'number', 'email', 'url', 'search', 'date', 'datetime', 'datetime-local', 'time', 'month', 'week']
-    return inputTypes.indexOf(elem.type) >= 0
-  }
-  return false
-}
+import {isTextInput} from "./util"
 
 class MenuBar {
   constructor() {
--- src/renderer/views/util.ts
+++ src/renderer/views/util.ts
@@ -5,3 +5,15 @@ function mouseOffsetPos(ev: {clientX: number, clientY: number}, element: Element
   const rect = element.getBoundingClientRect()
   return new Vec2(ev.clientX - rect.left + element.scrollLeft, ev.clientY - rect.top + element.scrollTop)
 }
+
+export
+function isTextInput(elem: Element) {
+  if (elem instanceof HTMLTextAreaElement) {
+    return true
+  }
+  if (elem instanceof HTMLInputElement) {
+    const inputTypes = ['text', 'password', 'number', 'email', 'url', 'search', 'date', 'datetime', 'datetime-local', 'time', 'month', 'week']
+    return inputTypes.indexOf(elem.type) >= 0
+  }
+  return false
+}
