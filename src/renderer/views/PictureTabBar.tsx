115e8ae15cd843113bd00b948b04bbcd0e553c6f
Ryohei Ikegami <iofg2100@gmail.com>
2016-10-18 16:23:19 +0900

    Style tabbar

--- src/renderer/views/PictureTabBar.tsx
+++ src/renderer/views/PictureTabBar.tsx
@@ -3,10 +3,11 @@ import {observer} from "mobx-react"
 import {AppState} from "../models/AppState"
 import Picture from "../models/Picture"
 import "../../styles/PictureTabBar.sass"
+const classNames = require("classnames")
 
-const PictureTab = observer((props: {picture: Picture}) => {
+const PictureTab = observer((props: {picture: Picture, current: boolean}) => {
   return (
-    <div className="PictureTab">
+    <div className={classNames("PictureTab", {"PictureTab-current": props.current})}>
       {props.picture.fileName}
     </div>
   )
@@ -15,10 +16,10 @@ const PictureTab = observer((props: {picture: Picture}) => {
 export
 const PictureTabBar = observer(() => {
   const appState = AppState.instance
-  const {pictures} = appState
+  const {pictures, currentPictureIndex} = appState
   return (
     <div className="PictureTabBar">
-      {pictures.map(p => <PictureTab picture={p} />)}
+      {pictures.map((p, i) => <PictureTab key={i} picture={p} current={i == currentPictureIndex}/>)}
     </div>
   )
 })
