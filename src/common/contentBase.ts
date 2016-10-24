9fc80c400da77fea62a21345429e656fea49485b
Ryohei Ikegami <iofg2100@gmail.com>
2016-10-24 13:40:05 +0900

    Fix dialog

--- src/common/contentBase.ts
+++ src/common/contentBase.ts
@@ -1,3 +1,5 @@
+const electron = require("electron")
+const {app} = process.type == "browser" ? electron : electron.remote
 
 export
-let contentBase = process.env.NODE_ENV == "development" ? "http://localhost:23000" : `file://${__dirname}/..`
+let contentBase = process.env.NODE_ENV == "development" ? "http://localhost:23000" : `file://${app.getAppPath()}/dist`
