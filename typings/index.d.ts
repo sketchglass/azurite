a64b79d2427ee7dcb6fef8c55fb8eb1d0e97e14c
Ryohei Ikegami <iofg2100@gmail.com>
2016-10-28 22:00:37 +0900

    Ignore typings

--- typings/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="globals/electron/github-electron/index.d.ts" />
-/// <reference path="globals/msgpack-lite/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/react-dom/index.d.ts" />
-/// <reference path="globals/react/index.d.ts" />
