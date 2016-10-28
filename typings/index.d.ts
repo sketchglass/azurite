6b31c04c179ee557083d1750070823824f5d8546
Ryohei Ikegami <iofg2100@gmail.com>
2016-10-28 21:36:54 +0900

    Install types with typings

---
+++ typings/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="globals/electron/github-electron/index.d.ts" />
+/// <reference path="globals/msgpack-lite/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/react-dom/index.d.ts" />
+/// <reference path="globals/react/index.d.ts" />
