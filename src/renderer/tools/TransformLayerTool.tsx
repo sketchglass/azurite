4077eacff07cd84462090eb409fa6f4b20861534
Ryohei Ikegami <iofg2100@gmail.com>
2016-10-31 20:05:06 +0900

    Get bounding rect

--- src/renderer/tools/TransformLayerTool.tsx
+++ src/renderer/tools/TransformLayerTool.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
+import {reaction, observable, computed} from "mobx"
 import {observer} from "mobx-react"
-import {Vec2, Transform} from "paintvec"
+import {Vec2, Rect, Transform} from "paintvec"
 import Tool from './Tool'
 import Waypoint from "../models/Waypoint"
 
@@ -8,8 +9,15 @@ import Waypoint from "../models/Waypoint"
 class TransformLayerOverlayUI extends React.Component<{tool: TransformLayerTool}, {}> {
   render() {
     const {tool} = this.props
+    const {boundingRect} = tool
+    let polygon: JSX.Element|undefined = undefined
+    if (boundingRect) {
+      const points = boundingRect.vertices().map(v => `${v.x},${v.y}`).join(" ")
+      polygon = <polygon points={points} />
+    }
     return (
       <g>
+        {polygon}
       </g>
     )
   }
@@ -18,6 +26,16 @@ class TransformLayerOverlayUI extends React.Component<{tool: TransformLayerTool}
 export default
 class TransformLayerTool extends Tool {
   name = "Move"
+
+  @computed get boundingRect() {
+    const {active, currentLayer} = this
+    console.log(active, currentLayer)
+    if (active && currentLayer && currentLayer.content.type == "image") {
+      const rect = currentLayer.content.tiledTexture.boundingRect()
+      console.log(rect)
+      return rect
+    }
+  }
 
   start(waypoint: Waypoint, rendererPos: Vec2) {
   }
