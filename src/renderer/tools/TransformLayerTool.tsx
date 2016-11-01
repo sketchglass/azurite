b4f7b945979aca71d908b0969ba54a441a06ab41
Ryohei Ikegami <iofg2100@gmail.com>
2016-11-01 17:24:03 +0900

    WIP implement hook

--- src/renderer/tools/TransformLayerTool.tsx
+++ src/renderer/tools/TransformLayerTool.tsx
@@ -2,8 +2,13 @@ import * as React from "react"
 import {reaction, observable, computed} from "mobx"
 import {observer} from "mobx-react"
 import {Vec2, Rect, Transform} from "paintvec"
-import Tool from './Tool'
+import {TextureDrawTarget} from "paintgl"
+import Layer from "../models/Layer"
+import {Tile} from "../models/TiledTexture"
+import {TileBlender} from "../models/LayerBlender"
 import Waypoint from "../models/Waypoint"
+import Tool from './Tool'
+import {context} from "../GLContext"
 
 @observer
 class TransformLayerOverlayUI extends React.Component<{tool: TransformLayerTool}, {}> {
@@ -26,14 +31,23 @@ class TransformLayerOverlayUI extends React.Component<{tool: TransformLayerTool}
   }
 }
 
+const tile = new Tile()
+const drawTarget = new TextureDrawTarget(context, tile.texture)
+
 export default
 class TransformLayerTool extends Tool {
   name = "Move"
 
-  @computed get boundingRect() {
+  @computed get currentContent() {
     const {active, currentLayer} = this
     if (active && currentLayer && currentLayer.content.type == "image") {
-      return currentLayer.content.tiledTexture.boundingRect()
+      return currentLayer.content
+    }
+  }
+
+  @computed get boundingRect() {
+    if (this.currentContent) {
+      return this.currentContent.tiledTexture.boundingRect()
     }
   }
 
@@ -48,5 +62,14 @@ class TransformLayerTool extends Tool {
 
   renderOverlayUI() {
     return <TransformLayerOverlayUI tool={this} />
+  }
+
+  hookLayerBlend(layer: Layer, tileKey: Vec2, tile: Tile|undefined, tileBlender: TileBlender) {
+    const content = this.currentContent
+    if (content && layer == content.layer) {
+      return false // TODO
+    } else {
+      return false
+    }
   }
 }
