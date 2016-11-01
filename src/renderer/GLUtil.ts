f5d37eaf933f0545e589af22030f22838c2f31ca
Ryohei Ikegami <iofg2100@gmail.com>
2016-11-01 17:40:11 +0900

    Add srcRect option

--- src/renderer/GLUtil.ts
+++ src/renderer/GLUtil.ts
@@ -9,14 +9,18 @@ const drawTextureModel = new Model(context, {
 })
 
 export
-function drawTexture(dst: DrawTarget, texture: Texture, params: {offset?: Vec2, dstRect?: Rect, blendMode?: BlendMode}) {
-  let {dstRect} = params
+function drawTexture(dst: DrawTarget, texture: Texture, params: {offset?: Vec2, dstRect?: Rect, srcRect?: Rect, blendMode?: BlendMode}) {
+  let {dstRect, srcRect} = params
   if (!dstRect) {
     const offset = params.offset || new Vec2(0)
     const {size} = texture
     dstRect = new Rect(offset, offset.add(size))
   }
+  const texRect = srcRect
+    ? new Rect(srcRect.topLeft.div(texture.size), srcRect.bottomRight.div(texture.size))
+    : new Rect(new Vec2(0), new Vec2(1))
   drawTextureShape.rect = dstRect
+  drawTextureShape.texCoords = texRect.vertices()
   drawTextureModel.blendMode = params.blendMode || "src-over"
   drawTextureModel.uniforms = {texture}
   dst.draw(drawTextureModel)
