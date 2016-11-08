7db16247f2815c2f70c565d1467c0cd8b4aaa7e8
minamorl <minamorl@minamorl.com>
2016-11-08 23:00:31 +0900

    Show transparent color with checked pattern

--- src/renderer/views/components/Palette.tsx
+++ src/renderer/views/components/Palette.tsx
@@ -1,10 +1,26 @@
 import React = require("react")
-import {HSVColor} from "../../../lib/Color"
+import {HSVColor, EmptyColor} from "../../../lib/Color"
 
 interface PaletteProps {
   palette: HSVColor[]
   paletteIndex: number
   onChange: (event: React.MouseEvent<Element>, index: number) => void
+}
+
+const TransparentRect = (props: {size: number}) => {
+  const style = {
+    fill: "rgba(122, 122, 122, 1)",
+    fillOpacity: 0.8
+  }
+  const {size} = props
+  return (
+    <svg width={`${size}`} height={`${size}`} viewBox={`0 0 ${size} ${size}`}>
+      <rect x={size/2} y={0} width={size/2} height={size/2} style={style}>
+      </rect>
+      <rect x={0} y={size/2} width={size/2} height={size/2} style={style}>
+      </rect>
+    </svg>
+  )
 }
 
 export default
@@ -20,13 +36,17 @@ function Palette(props: PaletteProps) {
   const rowElems = rows.map((row, y) => {
     const buttons = row.map((color, x) => {
       const i = y * rowLength + x
-      const style = {
-        backgroundColor: color.toString(),
-      }
       const onClick = (e: React.MouseEvent<Element>) => {
         props.onChange(e, i)
       }
-      return <div className="Palette-button" style={style} key={x} onClick={onClick} />
+      if (color.equals(EmptyColor)) {
+        return <div className="Palette-button Palette-button-transparent" key={x} onClick={onClick}><TransparentRect size={12} /></div>
+      } else {
+        const style = {
+          backgroundColor: color.toString(),
+        }
+        return <div className="Palette-button" style={style} key={x} onClick={onClick} />
+      }
     })
     return <div className="Palette-row" key={y}>{buttons}</div>
   })
