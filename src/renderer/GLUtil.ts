4e6e9e9603ad75a1b05789d6df561bc1fc9c8f46
Ryohei Ikegami <iofg2100@gmail.com>
2016-11-10 18:45:10 +0900

    Fix drawTexture with perspective transform

--- src/renderer/GLUtil.ts
+++ src/renderer/GLUtil.ts
@@ -1,11 +1,56 @@
 import {Vec2, Rect, Transform} from "paintvec"
-import {Model, Texture, RectShape, TextureShader, DrawTarget, TextureDrawTarget, PixelType, BlendMode} from "paintgl"
+import {Model, Texture, RectShape, Shader, TextureShader, DrawTarget, TextureDrawTarget, PixelType, BlendMode} from "paintgl"
 import {context} from "./GLContext"
 
 const drawTextureShape = new RectShape(context, {usage: "stream"})
 const drawTextureModel = new Model(context, {
   shape: drawTextureShape,
   shader: TextureShader,
+})
+
+class TexturePerspectiveShader extends Shader {
+  get vertexShader() {
+    return `
+      precision highp float;
+
+      uniform mat3 transform;
+      uniform mat3 transformPosition;
+      uniform mat3 transformTexCoord;
+      attribute vec2 aPosition;
+      attribute vec2 aTexCoord;
+      varying vec2 vPosition;
+      varying vec3 vTexCoord;
+
+      vec2 transform2(mat3 t, vec2 p) {
+        vec3 result = t * vec3(p, 1.0);
+        return result.xy / result.z;
+      }
+
+      void main(void) {
+        vPosition = aPosition;
+        vec2 pos = transform2(transformPosition, aPosition);
+        vTexCoord = transformTexCoord * vec3(pos, 1.0);
+        vec2 glPos = transform2(transform, pos);
+        gl_Position = vec4(glPos, 0.0, 1.0);
+      }
+    `;
+  }
+
+  get fragmentShader() {
+    return `
+      precision mediump float;
+      varying highp vec3 vTexCoord;
+      uniform sampler2D texture;
+      void main(void) {
+        gl_FragColor = texture2D(texture, vTexCoord.xy / vTexCoord.z);
+      }
+    `
+  }
+}
+
+const drawTexturePerspectiveModel = new Model(context, {
+  shape: drawTextureShape,
+  shader: TexturePerspectiveShader,
 })
 
 export
@@ -24,10 +69,23 @@ function drawTexture(dst: DrawTarget, src: Texture, params: {dstRect?: Rect, src
   if (!verticesEquals(drawTextureShape.texCoords, texCoords)) {
     drawTextureShape.texCoords = texCoords
   }
-  drawTextureModel.transform = params.transform || new Transform()
-  drawTextureModel.blendMode = params.blendMode || "src-over"
-  drawTextureModel.uniforms = {texture: src}
-  dst.draw(drawTextureModel)
+  const transform = params.transform || new Transform()
+  const blendMode = params.blendMode || "src-over"
+  if (transform.isAffine()) {
+    drawTextureModel.transform = transform
+    drawTextureModel.blendMode = blendMode
+    drawTextureModel.uniforms = {texture: src}
+    dst.draw(drawTextureModel)
+  } else {
+    const vertices = dstRect.vertices().map(v => v.transform(transform)) as [Vec2, Vec2, Vec2, Vec2]
+    const transformTexCoord = Transform.quadToQuad(vertices, texCoords)
+    if (!transformTexCoord) {
+      return
+    }
+    drawTexturePerspectiveModel.blendMode = blendMode
+    drawTexturePerspectiveModel.uniforms = {texture: src, transformPosition: transform, transformTexCoord}
+    dst.draw(drawTexturePerspectiveModel)
+  }
 }
 
 function verticesEquals(xs: Vec2[], ys: Vec2[]) {
