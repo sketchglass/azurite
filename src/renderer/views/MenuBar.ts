60dae3172fc5ceb4b94ee994cd401baa636fd63a
Ryohei Ikegami <iofg2100@gmail.com>
2016-11-15 20:44:36 +0900

    Add UndoState

--- src/renderer/views/MenuBar.ts
+++ src/renderer/views/MenuBar.ts
@@ -5,9 +5,9 @@ type BrowserWindow = Electron.BrowserWindow
 type MenuItemOptions = Electron.MenuItemOptions
 import {observable, computed, autorun} from "mobx"
 import {appState} from "../state/AppState"
+import {undoState} from "../state/UndoState"
 import Picture from "../models/Picture"
 import {PictureExportFormat} from "../services/PictureExport"
-import {isTextInput} from "./util"
 
 class MenuBar {
   constructor() {
@@ -15,19 +15,6 @@ class MenuBar {
       const menu = Menu.buildFromTemplate(this.render())
       Menu.setApplicationMenu(menu)
     })
-    window.addEventListener("focus", e => {
-      this.isTextInputFocused = isTextInput(document.activeElement)
-    }, true)
-  }
-
-  @computed get undoStack() {
-    const {modal, modalUndoStack, currentPictureState} = appState
-    if (modal) {
-      return modalUndoStack
-    }
-    if (currentPictureState) {
-      return currentPictureState.picture.undoStack
-    }
   }
 
   newPicture() {
@@ -52,62 +39,6 @@ class MenuBar {
 
   async close() {
     appState.closePicture(appState.currentPictureIndex)
-  }
-
-  @observable isTextInputFocused = false
-
-  @computed get canUndo() {
-    if (this.isTextInputFocused) {
-      return true
-    } else if (this.undoStack) {
-      return this.undoStack.isUndoable
-    }
-    return false
-  }
-
-  @computed get canRedo() {
-    if (this.isTextInputFocused) {
-      return true
-    } else if (this.undoStack) {
-      return this.undoStack.isRedoable
-    }
-    return false
-  }
-
-  @computed get undoName() {
-    if (this.undoStack) {
-      const {undoCommand} = this.undoStack
-      if (undoCommand) {
-        return undoCommand.title
-      }
-    }
-    return ""
-  }
-
-  @computed get redoName() {
-    if (this.undoStack) {
-      const {redoCommand} = this.undoStack
-      if (redoCommand) {
-        return redoCommand.title
-      }
-    }
-    return ""
-  }
-
-  undo() {
-    if (this.isTextInputFocused) {
-      remote.getCurrentWebContents().undo()
-    } else if (this.undoStack) {
-      this.undoStack.undo()
-    }
-  }
-
-  redo() {
-    if (this.isTextInputFocused) {
-      remote.getCurrentWebContents().redo()
-    } else if (this.undoStack) {
-      this.undoStack.redo()
-    }
   }
 
   async export(format: PictureExportFormat) {
@@ -200,16 +131,16 @@ class MenuBar {
       label: 'Edit',
       submenu: [
         {
-          label: `Undo ${this.undoName}`,
-          enabled: this.canUndo,
-          click: this.undo.bind(this),
-          accelerator: "CmdOrCtrl+Z"
+          label: `Undo ${undoState.undoName}`,
+          accelerator: "CmdOrCtrl+Z",
+          enabled: undoState.canUndo,
+          click: () => undoState.undo(),
         },
         {
-          label: `Redo ${this.redoName}`,
-          enabled: this.canRedo,
+          label: `Redo ${undoState.redoName}`,
           accelerator: process.platform === 'darwin' ? 'Shift+Command+Z' : 'Ctrl+Y',
-          click: this.redo.bind(this)
+          enabled: undoState.canRedo,
+          click: () => undoState.redo(),
         },
         {
           type: 'separator'
