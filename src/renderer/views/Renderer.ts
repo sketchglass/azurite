ed3d7d1292e4896635cfb484cc3ca9ac87ee6f02
Ryohei Ikegami <iofg2100@gmail.com>
2016-11-18 21:06:36 +0900

    Show canvas box shadow

--- src/renderer/views/Renderer.ts
+++ src/renderer/views/Renderer.ts
@@ -1,8 +1,41 @@
 import {observable, computed, reaction} from "mobx"
 import {Vec2, Rect, Transform} from "paintvec"
-import {Model, RectShape, TextureShader, CanvasDrawTarget, Color, TextureFilter} from "paintgl"
+import {Model, RectShape, Shader, TextureShader, CanvasDrawTarget, Color, TextureFilter} from "paintgl"
 import {context, canvas} from "../GLContext"
 import Picture from "../models/Picture"
+
+class BoxShadowShader extends Shader {
+  // http://madebyevan.com/shaders/fast-rounded-rectangle-shadows/
+  get fragmentShader() {
+    return `
+      precision mediump float;
+      varying vec2 vPosition;
+      uniform mat3 transformToPicture;
+      uniform vec2 pictureSize;
+
+      // This approximates the error function, needed for the gaussian integral
+      vec4 erf(vec4 x) {
+        vec4 s = sign(x), a = abs(x);
+        x = 1.0 + (0.278393 + (0.230389 + 0.078108 * (a * a)) * a) * a;
+        x *= x;
+        return s - s / (x * x);
+      }
+
+      // Return the mask for the shadow of a box from lower to upper
+      float boxShadow(vec2 lower, vec2 upper, vec2 point, float sigma) {
+        vec4 query = vec4(point - lower, point - upper);
+        vec4 integral = 0.5 + 0.5 * erf(query * (sqrt(0.5) / sigma));
+        return (integral.z - integral.x) * (integral.w - integral.y);
+      }
+
+      void main(void) {
+        vec2 pos = (transformToPicture * vec3(vPosition, 1.0)).xy;
+        float a = boxShadow(vec2(0.0), pictureSize, pos, 8.0);
+        gl_FragColor = vec4(0.0, 0.0, 0.0, a * 0.5);
+      }
+    `
+  }
+}
 
 export default
 class Renderer {
@@ -17,6 +50,14 @@ class Renderer {
   @observable size = new Vec2(100, 100)
   disposers: (() => void)[] = []
   background = new Color(46/255, 48/255, 56/255, 1)
+
+  readonly wholeShape = new RectShape(context, {
+    usage: "static",
+  })
+  readonly boxShadowModel = new Model(context, {
+    shape: this.wholeShape,
+    shader: BoxShadowShader,
+  })
 
   @computed get pictureSize() {
     if (this.picture) {
@@ -70,6 +111,7 @@ class Renderer {
       reaction(() => this.size, size => {
         canvas.width = size.width
         canvas.height = size.height
+        this.wholeShape.rect = new Rect(new Vec2(), size)
       }),
       reaction(() => [this.size, this.transformToPicture], () => {
         requestAnimationFrame(() => {
@@ -94,12 +136,19 @@ class Renderer {
     }
     drawTarget.clear(this.background)
     if (this.picture) {
+      console.log(this.picture.size)
+      this.boxShadowModel.uniforms = {
+        pictureSize: this.picture.size,
+        transformToPicture: this.transformToPicture,
+      }
+      drawTarget.draw(this.boxShadowModel)
+
       const filter: TextureFilter = this.picture.navigation.scale < 2 ? "bilinear" : "nearest"
       const texture = this.picture.layerBlender.blendedTexture
       if (texture.filter != filter) {
         texture.filter = filter
       }
-      drawTarget.transform = this.transformFromPicture
+      this.model.transform = this.transformFromPicture
       drawTarget.draw(this.model)
     }
   }
