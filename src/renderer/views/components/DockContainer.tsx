03fdae72721b839da7168633193de3cee0c08349
Ryohei Ikegami <iofg2100@gmail.com>
2016-11-21 15:41:03 +0900

    Implement tab dragging

--- src/renderer/views/components/DockContainer.tsx
+++ src/renderer/views/components/DockContainer.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import {observable, computed} from "mobx"
+import {observable, computed, action} from "mobx"
 import {observer} from "mobx-react"
 import * as classNames from "classnames"
 
@@ -13,6 +13,18 @@ class DockTabViewModel {
 
   render() {
     return this.root.renderTab(this.id)
+  }
+
+  @action moveToNewRow(row: DockRowViewModel, index: number) {
+    const oldIndex = this.row.tabs.indexOf(this)
+    this.row.tabs.splice(oldIndex, 1)
+    this.row.currentTabIndex = Math.min(this.row.currentTabIndex, this.row.tabs.length - 1)
+    if (this.row == row && oldIndex < index) {
+      index += 1
+    }
+    row.tabs.splice(index, 0, this)
+    row.currentTabIndex = row.tabs.indexOf(this)
+    this.row = row
   }
 
   @computed get title() {
@@ -64,6 +76,7 @@ class DockContainerViewModel {
   right = new DockColumnViewModel(this)
   @observable renderTab: (id: string) => JSX.Element
   @observable getTabName: (id: string) => string
+  @observable draggingTab: DockTabViewModel|undefined
 
   loadData(data: DockContainerData) {
     this.left.loadData(data.left)
@@ -107,18 +120,54 @@ class DockTab extends React.Component<{viewModel: DockTabViewModel}, {}> {
   }
 }
 
+export
+function dropIndexAt(element: HTMLElement, clientX: number) {
+  const children = Array.from(element.children)
+  for (const [i, child] of children.entries()) {
+    const rect = child.getBoundingClientRect()
+    if (i == 0 && clientX < rect.left) {
+      return 0
+    }
+    if (rect.left <= clientX && clientX <= rect.right) {
+      if (clientX <= rect.left + rect.width / 2) {
+        return i
+      } else {
+        return i + 1
+      }
+    }
+  }
+  return children.length
+}
+
 @observer
 class DockRow extends React.Component<{viewModel: DockRowViewModel}, {}> {
+  tabsElement: HTMLElement
+
   render() {
-    const {tabs, height} = this.props.viewModel
+    const {viewModel} = this.props
+    const {tabs, height} = viewModel
+    const onTabsDragOver = (e: React.DragEvent<HTMLElement>) => {
+      e.preventDefault()
+    }
+    const onTabsDrop = (e: React.DragEvent<HTMLElement>) => {
+      const index = dropIndexAt(this.tabsElement, e.clientX)
+      console.log(index)
+      if (viewModel.root.draggingTab) {
+        viewModel.root.draggingTab.moveToNewRow(viewModel, index)
+      }
+    }
     return (
       <div className="DockRow" style={{minHeight: `${height}px`}}>
-        <div className="DockRow_tabs">
+        <div className="DockRow_tabs" ref={e => this.tabsElement = e} onDragOver={onTabsDragOver} onDrop={onTabsDrop}>
           {
             tabs.map((t, i) => {
               const className = classNames("DockRow_tab", {"DockRow_tab-selected": t.selected})
               const onClick = () => this.props.viewModel.currentTabIndex = i
-              return <div key={t.title} className={className} onClick={onClick}>{t.title}</div>
+              const onDragStart = (e: React.DragEvent<HTMLElement>) => {
+                e.dataTransfer.setData("DockTab", "")
+                this.props.viewModel.root.draggingTab = t
+              }
+              return <div key={t.title} className={className} onClick={onClick} onDragStart={onDragStart} draggable={true}>{t.title}</div>
             })
           }
         </div>
