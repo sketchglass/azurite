ebdbe0986d729f73b95070a4c5a68d9f7737f528
Ryohei Ikegami <iofg2100@gmail.com>
2016-11-28 21:07:32 +0900

    Fix zoom level

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,6 +1,10 @@
 import React = require("react")
 import ReactDOM = require("react-dom")
+import {webFrame} from "electron"
 import App from "./views/App"
+
+webFrame.setVisualZoomLevelLimits(1, 1)
+webFrame.setLayoutZoomLevelLimits(1, 1)
 
 window.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(<App />, document.getElementById("app"))
