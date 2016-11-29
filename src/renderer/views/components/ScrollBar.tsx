62ba895a6bd469b4fdc2e36f30bc3d6b1116f549
Ryohei Ikegami <iofg2100@gmail.com>
2016-11-29 16:16:55 +0900

    Add ScrollBar

---
+++ src/renderer/views/components/ScrollBar.tsx
@@ -0,0 +1,39 @@
+import * as React from "react"
+import CSSVariables from "./CSSVariables"
+import * as classNames from "classnames"
+
+export
+enum ScrollBarDirection {
+  Vertical,
+  Horizontal,
+}
+
+interface ScrollBarProps {
+  direction: ScrollBarDirection
+  width: number
+  value: number
+  min: number
+  max: number
+  handleLength: number
+  onChange: (value: number) => void
+}
+
+function ScrollBar(props: ScrollBarProps) {
+  const {direction, width, value, min, max, handleLength, onChange} = props
+  const length = max - min + handleLength
+  const handleStart = value / length
+  const handleEnd = (value + handleLength) / length
+  const className = classNames("ScrollBar", {
+    "ScrollBar-vertical": direction == ScrollBarDirection.Vertical,
+    "ScrollBar-horizontal": direction == ScrollBarDirection.Horizontal,
+  })
+  return (
+    <CSSVariables width={width} handleStart={handleStart} handleEnd={handleEnd}>
+      <div className={className}>
+        <div className="ScrollBar_handle" />
+      </div>
+    </CSSVariables>
+  )
+}
+
+export default ScrollBar
