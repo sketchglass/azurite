80365d7e8842318732ccf253c1a85beaa8974277
Ryohei Ikegami <iofg2100@gmail.com>
2016-12-05 15:31:18 +0900

    Refactor DimensionSelect

--- src/renderer/state/DimensionSelectState.ts
+++ src/renderer/state/DimensionSelectState.ts
@@ -1,192 +1,158 @@
-import {observable, computed, action} from "mobx"
+import {observable, computed, action, reaction} from "mobx"
 import {PictureDimension} from "../models/Picture"
 import {MAX_PICTURE_SIZE} from "../../common/constants"
 
 export
-type DimensionUnit = "px" | "mm"
-
-interface DimensionPresetPx {
-  name: string
-  unit: "px"
-  widthPx: number
-  heightPx: number
-  dpi: number
-}
-
-interface DimensionPresetMm {
-  name: string
-  unit: "mm"
-  widthMm: number
-  heightMm: number
-  dpi: number
-}
+type DimensionUnit = "px" | "mm" | "inch"
 
 export
-type DimensionPreset = DimensionPresetPx | DimensionPresetMm
-
-export
-const dimensionPresets: DimensionPreset[] = [
-  {
-    name: "A4",
-    unit: "mm",
-    widthMm: 210,
-    heightMm: 297,
-    dpi: 144,
-  },
-  {
-    name: "A5",
-    unit: "mm",
-    widthMm: 148,
-    heightMm: 210,
-    dpi: 144,
-  },
-  {
-    name: "A6",
-    unit: "mm",
-    widthMm: 105,
-    heightMm: 148,
-    dpi: 144,
-  },
-  {
-    name: "1200 x 800",
-    unit: "px",
-    widthPx: 1200,
-    heightPx: 800,
-    dpi: 144,
-  }
-]
-
-function mmToPx(mm: number, dpi: number) {
-  return Math.round(mm / 25.4 * dpi)
-}
-
-function pxToMm(px: number, dpi: number) {
-  return Math.round(px / dpi * 25.4)
+interface DimensionPreset {
+  name: string
+  unit: DimensionUnit
+  width: number
+  height: number
+  dpi: number
 }
 
 export default
 class DimensionSelectState {
-  @observable widthMm = 0
-  @observable heightMm = 0
-  @observable widthPx = 0
-  @observable heightPx = 0
+  readonly presets: DimensionPreset[] = [
+    {
+      name: "A4",
+      unit: "mm",
+      width: 210,
+      height: 297,
+      dpi: 144,
+    },
+    {
+      name: "A5",
+      unit: "mm",
+      width: 148,
+      height: 210,
+      dpi: 144,
+    },
+    {
+      name: "A6",
+      unit: "mm",
+      width: 105,
+      height: 148,
+      dpi: 144,
+    },
+    {
+      name: "1200 x 800",
+      unit: "px",
+      width: 1200,
+      height: 800,
+      dpi: 144,
+    }
+  ]
+
+  @observable width = 0
+  @observable height = 0
   @observable dpi = 0
   @observable unit: DimensionUnit = "px"
   @observable ratio = 1
   @observable keepRatio = true
+  @observable lastSelectedPreset = -1
 
-  constructor() {
-    this.setPreset(dimensionPresets[0])
+  @computed get widthRounded() {
+    return Math.round(this.width)
+  }
+  @computed get heightRounded() {
+    return Math.round(this.height)
   }
 
-  @action setPreset(preset: DimensionPreset) {
-    if (preset.unit == "px") {
-      const {widthPx, heightPx, dpi, unit} = preset
-      this.setPx(widthPx, heightPx, dpi, unit, false)
-    } else {
-      const {widthMm, heightMm, dpi, unit} = preset
-      this.setMm(widthMm, heightMm, dpi, unit, false)
-    }
+  @computed get widthCurrentUnit() {
+    return this.fromPx(this.width, this.unit)
+  }
+  @computed get heightCurrentUnit() {
+    return this.fromPx(this.height, this.unit)
+  }
+
+  @computed get widthCurrentUnitRounded() {
+    return Math.round(this.widthCurrentUnit)
+  }
+  @computed get heightCurrentUnitRounded() {
+    return Math.round(this.heightCurrentUnit)
+  }
+
+  constructor() {
+    this.setPreset(0)
+    reaction(() => [this.width, this.height], () => {
+      this.lastSelectedPreset = -1
+    })
+  }
+
+  @action setPreset(index: number) {
+    const {width, height, dpi, unit} = this.presets[index]
+    const {keepRatio} = this
+    this.dpi = dpi
+    this.unit = unit
+    this.keepRatio = false
+    this.changeSizeCurrentUnit(width, height)
+    this.keepRatio = keepRatio
+    this.lastSelectedPreset = index
   }
 
   @computed get dimension(): PictureDimension {
     return {
-      width: this.widthPx,
-      height: this.heightPx,
+      width: this.widthRounded,
+      height: this.heightRounded,
       dpi: this.dpi,
     }
   }
 
   @computed get tooLarge() {
-    const {widthPx, heightPx} = this
-    return widthPx > MAX_PICTURE_SIZE || heightPx > MAX_PICTURE_SIZE
+    const {widthRounded, heightRounded} = this
+    return widthRounded > MAX_PICTURE_SIZE || heightRounded > MAX_PICTURE_SIZE
   }
 
   @computed get isValid() {
-    const {widthPx, heightPx} = this
-    return 0 < widthPx && 0 < heightPx && !this.tooLarge
+    const {widthRounded, heightRounded} = this
+    return 0 < widthRounded && 0 < heightRounded && !this.tooLarge
   }
 
-  @computed get currentPresetIndex() {
-    const {widthPx, heightPx, widthMm, heightMm, dpi} = this
-    for (const [i, preset] of dimensionPresets.entries()) {
-      if (preset.unit == "px") {
-        if (preset.widthPx == widthPx && preset.heightPx == heightPx && preset.dpi == dpi) {
-          return i
-        }
-      } else {
-        if (preset.widthMm == widthMm && preset.heightMm == heightMm && preset.dpi == dpi) {
-          return i
-        }
-      }
-    }
-    return -1
-  }
-
-  private setMm(widthMm: number, heightMm: number, dpi: number, unit: DimensionUnit, keepRatio: boolean) {
-    const widthPx = mmToPx(widthMm, dpi)
-    const heightPx = mmToPx(heightMm, dpi)
-    const ratio = keepRatio ? this.ratio : heightMm / widthMm
-    this.widthPx = widthPx
-    this.heightPx = heightPx
-    this.widthMm = widthMm
-    this.heightMm = heightMm
-    this.dpi = dpi
-    this.unit = unit
-    this.ratio = ratio
-  }
-
-  private setPx(widthPx: number, heightPx: number, dpi: number, unit: DimensionUnit, keepRatio: boolean) {
-    const widthMm = pxToMm(widthPx, dpi)
-    const heightMm = pxToMm(heightPx, dpi)
-    const ratio = keepRatio ? this.ratio : heightPx / widthPx
-    this.widthPx = widthPx
-    this.heightPx = heightPx
-    this.widthMm = widthMm
-    this.heightMm = heightMm
-    this.dpi = dpi
-    this.unit = unit
-    this.ratio = ratio
-  }
-
-  @action changeUnit(unit: DimensionUnit) {
-    this.unit = unit
-  }
-
-  @action changeWidth(width: number) {
-    const {dpi, unit, keepRatio} = this
-    if (unit == "mm") {
-      const height = keepRatio ? Math.round(width * this.ratio) : this.heightMm
-      this.setMm(width, height, dpi, unit, keepRatio)
-    } else {
-      const height = keepRatio ? Math.round(width * this.ratio) : this.heightPx
-      this.setPx(width, height, dpi, unit, keepRatio)
+  private toPx(value: number, unit: DimensionUnit) {
+    const {dpi} = this
+    switch (unit) {
+      case "px":
+        return value
+      case "mm":
+        return value / 25.4 * dpi
+      case "inch":
+        return value * dpi
     }
   }
 
-  @action changeHeight(height: number) {
-    const {dpi, unit, keepRatio} = this
-    if (unit == "mm") {
-      const width = keepRatio ? Math.round(height / this.ratio) : this.widthMm
-      this.setMm(width, height, dpi, unit, keepRatio)
-    } else {
-      const width = keepRatio ? Math.round(height / this.ratio) : this.widthPx
-      this.setPx(width, height, dpi, unit, keepRatio)
+  private fromPx(px: number, unit: DimensionUnit) {
+    const {dpi} = this
+    switch (unit) {
+      case "px":
+        return px
+      case "mm":
+        return px / dpi * 25.4
+      case "inch":
+        return px / dpi
     }
   }
 
-  @action changeDpi(dpi: number) {
-    const {unit, keepRatio} = this
-    if (unit == "mm") {
-      const {widthMm, heightMm} = this
-      this.setMm(widthMm, heightMm, dpi, unit, keepRatio)
-    } else {
-      const {widthPx, heightPx} = this
-      this.setPx(widthPx, heightPx, dpi, unit, keepRatio)
+  @action changeSizeCurrentUnit(width: number|undefined, height: number|undefined) {
+    let w = width != undefined ? this.toPx(width, this.unit) : undefined
+    let h = height != undefined ? this.toPx(height, this.unit) : undefined
+    if (w == undefined && h != undefined  && this.keepRatio) {
+      w = h / this.ratio
     }
-  }
-
-  @action changeKeepRatio(keepRatio: boolean) {
-    this.keepRatio = keepRatio
+    if (w != undefined && h == undefined  && this.keepRatio) {
+      h = w * this.ratio
+    }
+    if (w != undefined) {
+      this.width = w
+    }
+    if (h != undefined) {
+      this.height = h
+    }
+    if (!this.keepRatio) {
+      this.ratio = this.height / this.width
+    }
   }
 }
