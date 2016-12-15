99db60549548afaf149d3589b35040a20bda72e7
Ryohei Ikegami <iofg2100@gmail.com>
2016-12-15 17:57:44 +0900

    Refactor

--- src/renderer/tools/RectSelectTool.ts
+++ src/renderer/tools/RectSelectTool.ts
@@ -11,19 +11,20 @@ class RectSelectTool extends ShapeSelectTool {
   startPos = new Vec2()
   currentPos = new Vec2()
 
-  start(ev: ToolPointerEvent) {
-    this.startPos = this.currentPos = ev.rendererPos.round()
-    super.start(ev)
-  }
-  move(ev: ToolPointerEvent) {
-    this.currentPos = ev.rendererPos.round()
-    super.move(ev)
-  }
-
   get selectingRect() {
     if (this.drawing && !this.startPos.equals(this.currentPos)) {
       return Rect.fromTwoPoints(this.startPos, this.currentPos)
     }
+  }
+
+  start(ev: ToolPointerEvent) {
+    this.startPos = this.currentPos = ev.rendererPos.round()
+    super.start(ev)
+  }
+
+  move(ev: ToolPointerEvent) {
+    this.currentPos = ev.rendererPos.round()
+    super.move(ev)
   }
 
   drawShape(context: CanvasRenderingContext2D) {
