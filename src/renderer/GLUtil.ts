7daeef39dbe5c6af030eb20a39e5e0c0095f23a2
Ryohei Ikegami <iofg2100@gmail.com>
2016-12-25 01:04:40 +0900

    Move bicubic

--- src/renderer/GLUtil.ts
+++ src/renderer/GLUtil.ts
@@ -1,58 +1,15 @@
 import {Vec2, Rect, Transform} from "paintvec"
 import {ShapeModel, Texture, RectShape, QuadShape, textureShader, DrawTarget, TextureDrawTarget, BlendMode} from "paintgl"
 import {context} from "./GLContext"
-
-// http://www.java-gaming.org/index.php?topic=35123.0
-
-const textureBicubic = `
-  vec4 cubic(float v) {
-    vec4 n = vec4(1.0, 2.0, 3.0, 4.0) - v;
-    vec4 s = n * n * n;
-    float x = s.x;
-    float y = s.y - 4.0 * s.x;
-    float z = s.z - 4.0 * s.y + 6.0 * s.x;
-    float w = 6.0 - x - y - z;
-    return vec4(x, y, z, w) * (1.0/6.0);
-  }
-
-  vec4 textureBicubic(sampler2D sampler, vec2 texSize, vec2 texCoords) {
-    vec2 invTexSize = 1.0 / texSize;
-    texCoords = texCoords * texSize - 0.5;
-
-    vec2 fxy = fract(texCoords);
-    texCoords -= fxy;
-
-    vec4 xcubic = cubic(fxy.x);
-    vec4 ycubic = cubic(fxy.y);
-
-    vec4 c = texCoords.xxyy + vec2(-0.5, +1.5).xyxy;
-
-    vec4 s = vec4(xcubic.xz + xcubic.yw, ycubic.xz + ycubic.yw);
-    vec4 offset = c + vec4(xcubic.yw, ycubic.yw) / s;
-
-    offset *= invTexSize.xxyy;
-
-    vec4 sample0 = texture2D(sampler, offset.xz);
-    vec4 sample1 = texture2D(sampler, offset.yz);
-    vec4 sample2 = texture2D(sampler, offset.xw);
-    vec4 sample3 = texture2D(sampler, offset.yw);
-
-    float sx = s.x / (s.x + s.y);
-    float sy = s.z / (s.z + s.w);
-
-    return mix(
-      mix(sample3, sample2, sx), mix(sample1, sample0, sx)
-    , sy);
-  }
-`
+const glsl = require("glslify")
 
 const bicubicTextureShader = {
-  fragment: `
+  fragment: glsl`
+    #pragma glslify: bicubic = require('../lib/glsl/bicubic.glsl')
     uniform sampler2D texture;
     uniform vec2 texSize;
-    ${textureBicubic}
     void fragmentMain(vec2 pos, vec2 uv, out vec4 color) {
-      color = textureBicubic(texture, texSize, uv);
+      color = bicubic(texture, texSize, uv);
     }
   `
 }
@@ -139,7 +96,7 @@ function verticesEquals(xs: Vec2[], ys: Vec2[]) {
 }
 
 const inverseBilinearTextureShader = {
-  fragment: `
+  fragment: glsl`
     uniform sampler2D texture;
     uniform vec2 texSize;
     uniform float useBicubic;
@@ -150,9 +107,9 @@ const inverseBilinearTextureShader = {
     uniform vec2 uvOffset;
     uniform vec2 uvSize;
 
-    // http://www.iquilezles.org/www/articles/ibilinear/ibilinear.htm
+    #pragma glslify: bicubic = require('../lib/glsl/bicubic.glsl')
 
-    ${textureBicubic}
+    // http://www.iquilezles.org/www/articles/ibilinear/ibilinear.htm
 
     float cross(vec2 a, vec2 b) {
       return a.x*b.y - a.y*b.x;
@@ -197,7 +154,7 @@ const inverseBilinearTextureShader = {
 
     void fragmentMain(vec2 pos, vec2 texCoord, out vec4 color) {
       vec2 uv = uvOffset + invBilinear(pos, a, b, c, d) * uvSize;
-      color = (useBicubic == 1.0) ? textureBicubic(texture, texSize, uv) : texture2D(texture, uv);
+      color = (useBicubic == 1.0) ? bicubic(texture, texSize, uv) : texture2D(texture, uv);
     }
   `
 }
