9d22a71d08c280d0e95a21471df51c599f058c9e
Ryohei Ikegami <iofg2100@gmail.com>
2016-12-29 20:41:34 +0900

    Add some tests

--- src/test/models/PictureTest.ts
+++ src/test/models/PictureTest.ts
@@ -14,4 +14,23 @@ describe("Picture", () => {
       assert(picture.size.height == dimension.height)
     })
   })
+
+  describe("#rect", () => {
+    it("returns rwectangle with (0, 0) top-left and picture size", () => {
+      assert(picture.rect.left == 0)
+      assert(picture.rect.top == 0)
+      assert(picture.rect.width == dimension.width)
+      assert(picture.rect.height == dimension.height)
+    })
+  })
+
+  describe("#fileName", () => {
+    it("returns 'Untitled' when filePath is not set", () => {
+      assert(picture.fileName == "Untitled")
+    })
+    it("returns basename of filePath", () => {
+      picture.filePath = "/foo/bar/baz.azurite"
+      assert(picture.fileName == "baz.azurite")
+    })
+  })
 })
