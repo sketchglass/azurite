cb821abffa8ded98b207cdd46a68407dbc53a143
Ryohei Ikegami <iofg2100@gmail.com>
2016-12-29 19:11:24 +0900

    Add picture test

---
+++ src/test/models/PictureTest.ts
@@ -0,0 +1,16 @@
+import Picture from "../../renderer/models/Picture"
+
+describe("Picture", () => {
+  let dimension = {width: 1000, height: 2000, dpi: 72}
+  let picture: Picture
+  beforeEach(() => {
+    picture = new Picture(dimension)
+  })
+
+  describe("#size", () => {
+    it("returns width and height of picture", () => {
+      assert(picture.size.width == dimension.width)
+      assert(picture.size.height == dimension.height)
+    })
+  })
+})
