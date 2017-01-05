436df17baeaaf908b01c7149792c30e85e88008e
Ryohei Ikegami <iofg2100@gmail.com>
2017-01-05 21:10:24 +0900

    Add toElectronAccelerator

--- src/lib/KeyInput.ts
+++ src/lib/KeyInput.ts
@@ -5,4 +5,15 @@ export default
 class KeyInput {
   constructor(public modifiers: KeyModifier[], public key: string) {
   }
+
+  toElectronAccelerator() {
+    let key = this.key
+    if (key.length == 1) {
+      key = key.toUpperCase()
+    }
+    if (key == "+") {
+      key = "Plus"
+    }
+    return [...this.modifiers, key].join("+")
+  }
 }
