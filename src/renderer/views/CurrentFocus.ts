cf49e942790da0c20ad242718717af3f806274ab
Ryohei Ikegami <iofg2100@gmail.com>
2017-01-06 16:13:12 +0900

    Refactor edit actions

---
+++ src/renderer/views/CurrentFocus.ts
@@ -0,0 +1,22 @@
+import {observable, computed} from "mobx"
+import {isTextInput} from "./util"
+
+export default
+class CurrentFocus {
+  @observable element = document.activeElement
+
+  @computed get isTextInput() {
+    return isTextInput(this.element)
+  }
+
+  constructor() {
+    window.addEventListener("focus", () => this.onFocusChange(), true)
+    window.addEventListener("blur", () => this.onFocusChange(), true)
+  }
+
+  private onFocusChange() {
+    this.element = document.activeElement
+  }
+}
+
+export const currentFocus = new CurrentFocus()
