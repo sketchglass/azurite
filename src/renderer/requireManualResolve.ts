575b06a2aa4542ead3ec27f63e91293e7512c3c9
Ryohei Ikegami <iofg2100@gmail.com>
2017-01-16 00:07:40 +0900

    Workaround require

---
+++ src/renderer/requireManualResolve.ts
@@ -0,0 +1,11 @@
+import {remote} from "electron"
+const resolve = require("resolve")
+
+// workaround: looks like Electron can't resolve require path from JavaScripts loaded in http protocol
+// so we have to add alternative require that resolves path manually
+// todo: fix Electron
+window["requireManualResolve"] = (request: string) => {
+  const resolved = resolve.sync(request, {basedir: remote.app.getAppPath()})
+  console.log(resolved)
+  return window["require"](resolved)
+}
