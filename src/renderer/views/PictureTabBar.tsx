8eb697e4fec7cb32f9f6a6cb562604bb75414155
Ryohei Ikegami <iofg2100@gmail.com>
2017-01-16 19:32:14 +0900

    Do not reorder DOM during move

--- src/renderer/views/PictureTabBar.tsx
+++ src/renderer/views/PictureTabBar.tsx
@@ -9,16 +9,22 @@ import CSSVariables from "./components/CSSVariables"
 
 const TAB_WIDTH = 160
 
+interface PictureTabProps {
+  pictureState: PictureState
+  current: boolean
+  shift: number
+  onClick: () => void
+  onClose: () => void
+  onMove: (indexOffset: number) => void
+  onMoveEnd: () => void
+}
+
 @observer
-class PictureTab extends React.Component<{pictureState: PictureState, current: boolean, onClose: () => void}, {}> {
+class PictureTab extends React.Component<PictureTabProps, {}> {
   private element: HTMLElement
   @observable private dragged = false
   private originalX = 0
   @observable private offset = 0
-
-  private onClick = () => {
-    appState.currentPictureIndex = appState.pictureStates.indexOf(this.props.pictureState)
-  }
 
   private onCloseClick = (e: React.MouseEvent<HTMLSpanElement>) => {
     e.stopPropagation()
@@ -35,58 +41,26 @@ class PictureTab extends React.Component<{pictureState: PictureState, current: b
     }
     const x = Math.round(e.clientX)
     const offset = this.offset = x - this.originalX
-    if (TAB_WIDTH / 2 < offset) {
-      this.moveForward()
-    } else if (offset < -TAB_WIDTH / 2) {
-      this.moveBackward()
-    }
+    this.props.onMove(Math.round(offset / TAB_WIDTH))
   })
   private onPointerUp = action((e: PointerEvent) => {
     this.dragged = false
     this.offset = 0
+    this.props.onMoveEnd()
   })
 
-  private moveForward() {
-    const {pictureState} = this.props
-    const index = appState.pictureStates.indexOf(pictureState)
-    const current = index == appState.currentPictureIndex
-    if (0 <= index && index < appState.pictureStates.length - 1) {
-      appState.pictureStates.splice(index, 1)
-      appState.pictureStates.splice(index + 1, 0, pictureState)
-      if (current) {
-        ++appState.currentPictureIndex
-      }
-      this.originalX += TAB_WIDTH
-      this.offset -= TAB_WIDTH
-    }
-  }
-
-  private moveBackward() {
-    const {pictureState} = this.props
-    const index = appState.pictureStates.indexOf(pictureState)
-    const current = index == appState.currentPictureIndex
-    if (0 < index) {
-      appState.pictureStates.splice(index, 1)
-      appState.pictureStates.splice(index - 1, 0, pictureState)
-      if (current) {
-        --appState.currentPictureIndex
-      }
-      this.originalX -= TAB_WIDTH
-      this.offset += TAB_WIDTH
-    }
-  }
-
   render() {
-    const {pictureState, current} = this.props
-    const {offset, dragged} = this
+    const {pictureState, current, onClick, shift} = this.props
+    const {dragged} = this
     const className = classNames("PictureTab", {
       "PictureTab-current": current,
       "PictureTab-dragged": dragged,
     })
+    const offset = this.offset + shift * TAB_WIDTH
     return (
       <CSSVariables offset={offset + "px"} width={TAB_WIDTH + "px"}>
         <PointerEvents onPointerDown={this.onPointerDown} onPointerMove={this.onPointerMove} onPointerUp={this.onPointerUp}>
-          <div className={className} onClick={this.onClick} ref={e => this.element = e}>
+          <div className={className} onClick={onClick} ref={e => this.element = e}>
             <span className="PictureTab_title">{pictureState.picture.fileName}</span>
             <span className="PictureTab_close" onClick={this.onCloseClick}>x</span>
           </div>
@@ -99,15 +73,35 @@ class PictureTab extends React.Component<{pictureState: PictureState, current: b
 @observer
 export
 class PictureTabBar extends React.Component<{hidden: boolean}, {}> {
+  @observable private moving = false
+  @observable private movingIndex = 0
+  @observable private movingIndexOffset = 0
+
   render() {
     const {pictureStates, currentPictureIndex} = appState
     return (
       <div className="PictureTabBar" hidden={this.props.hidden}>
         {
           pictureStates.map((p, i) => {
+            const shiftRight = this.moving && this.movingIndex + this.movingIndexOffset <= i && i < this.movingIndex
+            const shiftLeft = this.moving && this.movingIndex < i && i <= this.movingIndex + this.movingIndexOffset
+            const shift = shiftRight ? 1 : shiftLeft ? -1 : 0
+            const onClick = () => appState.currentPictureIndex = i
             const onClose = () => appState.closePicture(i)
+            const onMove = action((indexOffset: number) => {
+              this.moving = true
+              this.movingIndex = i
+              this.movingIndexOffset = indexOffset
+            })
+            const onMoveEnd = action(() => {
+              this.moving = false
+              if (this.movingIndexOffset != 0) {
+                const [state] = appState.pictureStates.splice(this.movingIndex, 1)
+                appState.pictureStates.splice(this.movingIndex + this.movingIndexOffset, 0, state)
+              }
+            })
             const current = i == currentPictureIndex
-            return <PictureTab key={p.picture.id} pictureState={p} current={current} onClose={onClose}/>
+            return <PictureTab key={p.picture.id} pictureState={p} shift={shift} current={current} onClose={onClose} onClick={onClick} onMove={onMove} onMoveEnd={onMoveEnd}/>
           })
         }
         <div className="PictureTabFill"></div>
