f1b4f597bd1961dad5c8746f03cd3a6e60b41eb7
Ryohei Ikegami <iofg2100@gmail.com>
2017-01-16 17:10:19 +0900

    Implement tab move

--- src/renderer/views/PictureTabBar.tsx
+++ src/renderer/views/PictureTabBar.tsx
@@ -1,51 +1,90 @@
 import * as React from "react"
+import {action} from "mobx"
 import {observer} from "mobx-react"
 import {appState} from "../state/AppState"
-import Picture from "../models/Picture"
+import {PictureState} from "../state/PictureState"
 const classNames = require("classnames")
 import PointerEvents from "./components/PointerEvents"
 import CSSVariables from "./components/CSSVariables"
 
-class PictureTab extends React.Component<{picture: Picture, current: boolean, onClick: () => void, onClose: () => void}, {offset: number}> {
-  state = {offset: 0}
+const TAB_WIDTH = 160
+
+class PictureTab extends React.Component<{pictureState: PictureState, current: boolean, onClose: () => void}, {}> {
   private element: HTMLElement
   private dragged = false
   private originalX = 0
+  private offset = 0
+
+  private onClick = () => {
+    appState.currentPictureIndex = appState.pictureStates.indexOf(this.props.pictureState)
+  }
 
   private onCloseClick = (e: React.MouseEvent<HTMLSpanElement>) => {
     e.stopPropagation()
     this.props.onClose()
   }
-  private onPointerDown = (e: PointerEvent) => {
-    e.preventDefault()
+  private onPointerDown = action((e: PointerEvent) => {
     this.element.setPointerCapture(e.pointerId)
     this.originalX = Math.round(e.clientX)
     this.dragged = true
-  }
-  private onPointerMove = (e: PointerEvent) => {
-    e.preventDefault()
+  })
+  private onPointerMove = action((e: PointerEvent) => {
     if (!this.dragged) {
       return
     }
     const x = Math.round(e.clientX)
-    const offset = x - this.originalX
-    this.setState({offset})
-  }
+    const offset = this.offset = x - this.originalX
+    if (TAB_WIDTH / 2 < offset) {
+      this.moveForward()
+    } else if (offset < -TAB_WIDTH / 2) {
+      this.moveBackward()
+    }
+    this.forceUpdate()
+  })
   private onPointerUp = (e: PointerEvent) => {
-    e.preventDefault()
     this.dragged = false
-    this.setState({offset: 0})
+    this.offset = 0
+    this.forceUpdate()
+  }
+
+  private moveForward() {
+    const {pictureState} = this.props
+    const index = appState.pictureStates.indexOf(pictureState)
+    const current = index == appState.currentPictureIndex
+    if (0 <= index && index < appState.pictureStates.length - 1) {
+      appState.pictureStates.splice(index, 1)
+      appState.pictureStates.splice(index + 1, 0, pictureState)
+      if (current) {
+        ++appState.currentPictureIndex
+      }
+      this.originalX += TAB_WIDTH
+      this.offset -= TAB_WIDTH
+    }
+  }
+
+  private moveBackward() {
+    const {pictureState} = this.props
+    const index = appState.pictureStates.indexOf(pictureState)
+    const current = index == appState.currentPictureIndex
+    if (0 < index) {
+      appState.pictureStates.splice(index, 1)
+      appState.pictureStates.splice(index - 1, 0, pictureState)
+      if (current) {
+        --appState.currentPictureIndex
+      }
+      this.originalX -= TAB_WIDTH
+      this.offset += TAB_WIDTH
+    }
   }
 
   render() {
-    const {picture, current, onClick} = this.props
-    const {offset} = this.state
-    console.log(offset)
+    const {pictureState, current} = this.props
+    const {offset} = this
     return (
-      <CSSVariables offset={offset + "px"}>
+      <CSSVariables offset={offset + "px"} width={TAB_WIDTH + "px"}>
         <PointerEvents onPointerDown={this.onPointerDown} onPointerMove={this.onPointerMove} onPointerUp={this.onPointerUp}>
-          <div className={classNames("PictureTab", {"PictureTab-current": current})} onClick={onClick} ref={e => this.element = e}>
-            <span className="PictureTab_title">{picture.fileName}</span>
+          <div className={classNames("PictureTab", {"PictureTab-current": current})} onClick={this.onClick} ref={e => this.element = e}>
+            <span className="PictureTab_title">{pictureState.picture.fileName}</span>
             <span className="PictureTab_close" onClick={this.onCloseClick}>x</span>
           </div>
         </PointerEvents>
@@ -61,10 +100,9 @@ const PictureTabBar = observer((props: {hidden: boolean}) => {
     <div className="PictureTabBar" hidden={props.hidden}>
       {
         pictureStates.map((p, i) => {
-          const onClick = () => appState.currentPictureIndex = i
           const onClose = () => appState.closePicture(i)
           const current = i == currentPictureIndex
-          return <PictureTab key={i} picture={p.picture} current={current} onClick={onClick} onClose={onClose}/>
+          return <PictureTab key={p.picture.id} pictureState={p} current={current} onClose={onClose}/>
         })
       }
       <div className="PictureTabFill"></div>
