bac2935f60fe73c247f6efe4df6d5b6d7710f7ff
Ryohei Ikegami <iofg2100@gmail.com>
2017-01-16 16:30:23 +0900

    Implement tab drag

--- src/renderer/views/PictureTabBar.tsx
+++ src/renderer/views/PictureTabBar.tsx
@@ -4,22 +4,52 @@ import {appState} from "../state/AppState"
 import Picture from "../models/Picture"
 const classNames = require("classnames")
 import PointerEvents from "./components/PointerEvents"
+import CSSVariables from "./components/CSSVariables"
 
-class PictureTab extends React.Component<{picture: Picture, current: boolean, onClick: () => void, onClose: () => void}, {}> {
+class PictureTab extends React.Component<{picture: Picture, current: boolean, onClick: () => void, onClose: () => void}, {offset: number}> {
+  state = {offset: 0}
+  private element: HTMLElement
+  private dragged = false
+  private originalX = 0
+
   private onCloseClick = (e: React.MouseEvent<HTMLSpanElement>) => {
     e.stopPropagation()
     this.props.onClose()
   }
+  private onPointerDown = (e: PointerEvent) => {
+    e.preventDefault()
+    this.element.setPointerCapture(e.pointerId)
+    this.originalX = Math.round(e.clientX)
+    this.dragged = true
+  }
+  private onPointerMove = (e: PointerEvent) => {
+    e.preventDefault()
+    if (!this.dragged) {
+      return
+    }
+    const x = Math.round(e.clientX)
+    const offset = x - this.originalX
+    this.setState({offset})
+  }
+  private onPointerUp = (e: PointerEvent) => {
+    e.preventDefault()
+    this.dragged = false
+    this.setState({offset: 0})
+  }
+
   render() {
     const {picture, current, onClick} = this.props
-    const {onCloseClick} = this
+    const {offset} = this.state
+    console.log(offset)
     return (
-      <PointerEvents>
-        <div className={classNames("PictureTab", {"PictureTab-current": current})} onClick={onClick}>
-          <span className="PictureTab_title">{picture.fileName}</span>
-          <span className="PictureTab_close" onClick={onCloseClick}>x</span>
-        </div>
-      </PointerEvents>
+      <CSSVariables offset={offset + "px"}>
+        <PointerEvents onPointerDown={this.onPointerDown} onPointerMove={this.onPointerMove} onPointerUp={this.onPointerUp}>
+          <div className={classNames("PictureTab", {"PictureTab-current": current})} onClick={onClick} ref={e => this.element = e}>
+            <span className="PictureTab_title">{picture.fileName}</span>
+            <span className="PictureTab_close" onClick={this.onCloseClick}>x</span>
+          </div>
+        </PointerEvents>
+      </CSSVariables>
     )
   }
 }
