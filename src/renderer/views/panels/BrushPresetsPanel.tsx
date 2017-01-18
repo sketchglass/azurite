5416f2ba094a36ba6593650e1ab33d44a01c8142
Ryohei Ikegami <iofg2100@gmail.com>
2017-01-18 14:19:34 +0900

    Make brush preset selectable

--- src/renderer/views/panels/BrushPresetsPanel.tsx
+++ src/renderer/views/panels/BrushPresetsPanel.tsx
@@ -1,16 +1,24 @@
 import * as React from "react"
+import {action} from "mobx"
 import {observer} from "mobx-react"
 import {brushPresetManager} from "../../app/BrushPresetManager"
 import {BrushPreset} from "../../brush/BrushPreset"
+import * as classNames from "classnames"
 
-function BrushPresetItem(props: {preset: BrushPreset}) {
-  const {title} = props.preset
+const BrushPresetItem = observer((props: {preset: BrushPreset, index: number}) => {
+  const {preset, index} = props
+  const {title} = preset
+  const selected = brushPresetManager.currentPresetIndex == index
+  const className = classNames("BrushPresetItem", {"BrushPresetItem-selected": selected})
+  const onClick = action(() => {
+    brushPresetManager.currentPresetIndex = index
+  })
   return (
-    <div className="BrushPresetItem">
+    <div className={className} onClick={onClick}>
       {title}
     </div>
   )
-}
+})
 
 @observer
 export default class BrushPresetsPanel extends React.Component<{}, {}> {
@@ -18,7 +26,7 @@ export default class BrushPresetsPanel extends React.Component<{}, {}> {
     const {presets} = brushPresetManager
     return (
       <div className="BrushPresetsPanel">{
-        presets.map((preset, i) => <BrushPresetItem key={i} preset={preset} />)
+        presets.map((preset, i) => <BrushPresetItem key={i} preset={preset} index={i} />)
       }</div>
     )
   }
