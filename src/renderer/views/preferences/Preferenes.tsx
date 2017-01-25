87c4ca607dde9c8e79298d79ecce609792af951d
Ryohei Ikegami <iofg2100@gmail.com>
2017-01-25 17:44:29 +0900

    Handle preferencesOpen

--- src/renderer/views/preferences/Preferenes.tsx
+++ src/renderer/views/preferences/Preferenes.tsx
@@ -1,8 +1,9 @@
 import * as React from "react"
 import {observer} from "mobx-react"
+import PreferencesViewModel from "../../viewmodels/PreferencesViewModel"
 
 @observer
 export default
 class Preferences extends React.Component<{}, {}> {
-
+  viewModel = new PreferencesViewModel()
 }
