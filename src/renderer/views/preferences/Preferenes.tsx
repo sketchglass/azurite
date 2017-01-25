38bd79adc3b00cd3a9ea27ab1fa43183eff6fed3
Ryohei Ikegami <iofg2100@gmail.com>
2017-01-25 18:34:21 +0900

    Add simple preferences

--- src/renderer/views/preferences/Preferenes.tsx
+++ src/renderer/views/preferences/Preferenes.tsx
@@ -8,8 +8,21 @@ class Preferences extends React.Component<{}, {}> {
   viewModel = new PreferencesViewModel()
 
   render() {
+    const {undoGroupingInterval} = this.viewModel
+
+    const onUndoGroupingIntervalChange = (e: React.FormEvent<HTMLInputElement>) => {
+      const value = parseInt((e.target as HTMLInputElement).value)
+      this.viewModel.undoGroupingInterval = value
+      this.viewModel.notifyChange()
+    }
+
     return (
       <div className="Preferences">
+        <div className="PreferencesPane">
+          <div>Undo Grouping Interval</div>
+          <input type="number" value={undoGroupingInterval} onChange={onUndoGroupingIntervalChange} />
+          <div>milliseconds</div>
+        </div>
       </div>
     )
   }
