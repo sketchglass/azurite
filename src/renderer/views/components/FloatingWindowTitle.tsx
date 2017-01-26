e4350e2ed125c433b70c4e0039135a53b751a803
Ryohei Ikegami <iofg2100@gmail.com>
2017-01-26 17:46:51 +0900

    Close button

--- src/renderer/views/components/FloatingWindowTitle.tsx
+++ src/renderer/views/components/FloatingWindowTitle.tsx
@@ -1,13 +1,17 @@
 import * as React from "react"
+import {remote} from "electron"
 
 export default
 function FloatingWindowTitle(props: {title: string}) {
   const {title} = props
   const className = `FloatingWindowTitle FloatingWindowTitle-${process.platform}`
+  const onClose = () => {
+    remote.getCurrentWindow().close()
+  }
   return (
     <div className={className}>
       {title}
-      <div className="FloatingWindowTitle_close" />
+      <div className="FloatingWindowTitle_close" onClick={onClose} />
     </div>
   )
 }
