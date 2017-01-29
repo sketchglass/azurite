44287a6bc4f582b026657352c2fee9f2d820cea1
Ryohei Ikegami <iofg2100@gmail.com>
2017-01-29 16:07:09 +0900

    Fix typo Preferences

--- src/renderer/views/preferences/PreferencesIndex.tsx
+++ src/renderer/views/preferences/PreferencesIndex.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 import {ipcRenderer} from "electron"
-import Preferences from "./Preferenes"
+import Preferences from "./Preferences"
 import IPCChannels from "../../../common/IPCChannels"
 import {PreferencesData} from "../../viewmodels/PreferencesViewModel"
 import "../../../styles/main.css"
