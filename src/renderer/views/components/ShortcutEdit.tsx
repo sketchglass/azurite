d2c911233411c2838a1c66c3598e2a8a8fecfdef
Ryohei Ikegami <iofg2100@gmail.com>
2017-01-30 15:12:49 +0900

    Add ShortcutEdit

---
+++ src/renderer/views/components/ShortcutEdit.tsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+import KeyInput from "../../../lib/KeyInput"
+
+export default
+class ShortcutEdit extends React.Component<{shortcut: KeyInput}, {}> {
+  render() {
+    return (
+      <div className="ShortcutEdit">
+      </div>
+    )
+  }
+}
