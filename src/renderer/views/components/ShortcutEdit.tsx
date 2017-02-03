2e63d10f2263dd05752479251744c09e50b9ece3
Ryohei Ikegami <iofg2100@gmail.com>
2017-02-03 13:46:39 +0900

    Add Shortcut edit component

--- src/renderer/views/components/ShortcutEdit.tsx
+++ src/renderer/views/components/ShortcutEdit.tsx
@@ -1,12 +1,43 @@
 import * as React from "react"
-import KeyInput from "../../../lib/KeyInput"
+import KeyInput, {KeyModifier} from "../../../lib/KeyInput"
+
+interface ShortcutEditProps {
+  shortcut: KeyInput|undefined
+  onChange: (shortcut: KeyInput|undefined) => void
+}
 
 export default
-class ShortcutEdit extends React.Component<{shortcut: KeyInput}, {}> {
+class ShortcutEdit extends React.Component<ShortcutEditProps, {}> {
+  private modifiers = new Set<KeyModifier>()
+  private key: string|undefined
+
   render() {
+    const {shortcut} = this.props
     return (
-      <div className="ShortcutEdit">
+      <div className="ShortcutEdit" tabIndex={-1} onKeyDown={this.onKeyDown} onFocus={this.onFocus} >
+        {shortcut ? shortcut.toElectronAccelerator() : ""}
       </div>
     )
+  }
+
+  private onFocus = () => {
+    this.modifiers.clear()
+    this.key = undefined
+  }
+
+  private onKeyDown = (e: React.KeyboardEvent<HTMLElement>) => {
+    const {key} = e
+    if (this.key == undefined) {
+      this.key = key
+    } else if (key == "Control" || key == "Meta" || key == "Alt" || key == "Shift") {
+      this.modifiers.add(key)
+    } else {
+      this.key = key
+    }
+    if (this.key) {
+      const shortcut = new KeyInput([...this.modifiers], this.key)
+      this.props.onChange(shortcut)
+    }
+    e.preventDefault()
   }
 }
--- src/renderer/views/dialogs/ToolShortcutsDialog.tsx
+++ src/renderer/views/dialogs/ToolShortcutsDialog.tsx
@@ -1,7 +1,9 @@
 import React = require("react")
+import {observable} from "mobx"
 import {observer} from "mobx-react"
 import DialogContainer from "./DialogContainer"
-import {KeyInputData} from "../../../lib/KeyInput"
+import KeyInput, {KeyInputData} from "../../../lib/KeyInput"
+import ShortcutEdit from "../components/ShortcutEdit"
 
 interface ToolShortcutsDialogProps {
   onReadyShow: () => void
@@ -12,9 +14,29 @@ interface ToolShortcutsDialogProps {
 @observer
 export default
 class ToolShortcutsDialog extends React.Component<ToolShortcutsDialogProps, {}> {
+  @observable shortcut: KeyInput|undefined
+  @observable tempShortcut: KeyInput|undefined
+
+  constructor(props: ToolShortcutsDialogProps) {
+    super(props)
+    const [shortcut, tempShortcut] = this.props.init
+    this.shortcut = shortcut && KeyInput.fromData(shortcut)
+    this.tempShortcut = tempShortcut && KeyInput.fromData(tempShortcut)
+  }
+
   render() {
     return (
       <DialogContainer title="Tool Shortcuts" okText="OK" canOK={true} onOK={this.onOK} onCancel={this.onCancel}>
+        <table>
+          <tr>
+            <td>Shortcut</td>
+            <td><ShortcutEdit shortcut={this.shortcut} onChange={s => this.shortcut = s} /></td>
+          </tr>
+          <tr>
+            <td>Temp Shortcut</td>
+            <td><ShortcutEdit shortcut={this.tempShortcut} onChange={s => this.tempShortcut = s} /></td>
+          </tr>
+        </table>
       </DialogContainer>
     )
   }
