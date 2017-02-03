8775994b7cb14661cbe344ce098a430ec00c146c
Ryohei Ikegami <iofg2100@gmail.com>
2017-02-03 13:51:57 +0900

    Better key handling

--- src/renderer/views/components/ShortcutEdit.tsx
+++ src/renderer/views/components/ShortcutEdit.tsx
@@ -8,9 +8,6 @@ interface ShortcutEditProps {
 
 export default
 class ShortcutEdit extends React.Component<ShortcutEditProps, {}> {
-  private modifiers = new Set<KeyModifier>()
-  private key: string|undefined
-
   render() {
     const {shortcut} = this.props
     return (
@@ -21,23 +18,20 @@ class ShortcutEdit extends React.Component<ShortcutEditProps, {}> {
   }
 
   private onFocus = () => {
-    this.modifiers.clear()
-    this.key = undefined
+    this.props.onChange(undefined)
   }
 
   private onKeyDown = (e: React.KeyboardEvent<HTMLElement>) => {
     const {key} = e
-    if (this.key == undefined) {
-      this.key = key
-    } else if (key == "Control" || key == "Meta" || key == "Alt" || key == "Shift") {
-      this.modifiers.add(key)
-    } else {
-      this.key = key
+    const modifiers = new Set<KeyModifier>()
+    if (!["Shift", "Alt", "Control", "Meta"].includes(key)) {
+      e.shiftKey && modifiers.add("Shift")
+      e.altKey && modifiers.add("Alt")
+      e.ctrlKey && modifiers.add("Control")
+      e.metaKey && modifiers.add("Meta")
     }
-    if (this.key) {
-      const shortcut = new KeyInput([...this.modifiers], this.key)
-      this.props.onChange(shortcut)
-    }
+    const shortcut = new KeyInput([...modifiers], key)
+    this.props.onChange(shortcut)
     e.preventDefault()
   }
 }
